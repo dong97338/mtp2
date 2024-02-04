2f35bd3bb1cd7b057a6f00d855e16a1f3418f9fd
dong97338 <dong97338@gmail.com>
2024-02-04 18:41:44 +0900

    fix: rollback

--- app/layout.js
+++ app/layout.js
@@ -3,5 +3,4 @@
 import "./globals.css";
 import { ThemeProvider } from "@material-tailwind/react";
 
-export default Layout = ({ children }) => <html><body className="flex min-h-screen flex-col items-center justify-between p-24"><ThemeProvider>{children}</ThemeProvider></body></html>
-Layout.displayName = 'Layout';
+export default ({ children }) => <html><body className="flex min-h-screen flex-col items-center justify-between p-24"><ThemeProvider>{children}</ThemeProvider></body></html>
