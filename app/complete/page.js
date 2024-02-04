6901f8f78e9c8e070a13c6e0b0eba1d725cf1a87
dong97338 <dong97338@gmail.com>
2024-02-04 20:44:25 +0900

    feat: delete eventlistener

--- app/complete/page.js
+++ app/complete/page.js
@@ -9,24 +9,9 @@ const dummy = ['https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixl
   "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"]
 
 const Page = () => {
-
-  // 모바일 환경인지 검사하는 함수
   const [isMobile, setIsMobile] = useState(false);
-
-  // 모바일 환경인지 검사하는 함수를 useEffect 내에서 실행
+  useEffect(() => { setIsMobile(window.innerHeight > window.innerWidth) }, []);
   useEffect(() => {
-    const checkIfMobile = () => window.innerHeight > window.innerWidth;
-    setIsMobile(checkIfMobile());
-
-    // 화면 크기가 변경될 때마다 모바일 여부를 다시 검사
-    window.addEventListener('resize', () => setIsMobile(checkIfMobile()));
-
-    // 컴포넌트가 언마운트 될 때 이벤트 리스너 제거
-    return () => window.removeEventListener('resize', () => setIsMobile(checkIfMobile()));
-  }, []);
-
-  useEffect(() => {
-    // 모바일 환경 또는 데스크탑 환경에 따른 처리
     console.log(isMobile ? "모바일 환경입니다." : "데스크탑 환경입니다.");
   }, [isMobile]);
 
