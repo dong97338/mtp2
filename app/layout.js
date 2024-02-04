6bc5444adf8a6fc1b6f772baf5d59f198d0c6030
dong97338 <dong97338@gmail.com>
2024-02-04 18:32:57 +0900

    fix: add name

--- app/layout.js
+++ app/layout.js
@@ -3,4 +3,4 @@
 import "./globals.css";
 import { ThemeProvider } from "@material-tailwind/react";
 
-export default ({ children }) => <html><body className="flex min-h-screen flex-col items-center justify-between p-24"><ThemeProvider>{children}</ThemeProvider></body></html>
+export default Layout = ({ children }) => <html><body className="flex min-h-screen flex-col items-center justify-between p-24"><ThemeProvider>{children}</ThemeProvider></body></html>
