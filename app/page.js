4e179b5bc495dbbb17ae1e7d28d197f5acc6ce62
dong97338 <dong97338@gmail.com>
2024-02-06 09:45:35 +0900

    refactor: add db

--- app/page.js
+++ app/page.js
@@ -11,11 +11,15 @@ import {
 import { CameraIcon, BookOpenIcon, UserIcon, PaintBrushIcon, VideoCameraIcon } from "@heroicons/react/24/outline";
 import Webcam from "react-webcam";
 
+const db = { story: ['디즈니', '지브리', '해리포터'], gender: ['남자', '여자'] }
+const steps = [[CameraIcon, "사진"], [PaintBrushIcon, "스토리"], [UserIcon, "성별"]]
+const backgroundImages = ['/transparent.png', '/autumn.jpg', '/dawn.jpg', '/snow.jpg', '/space.jpg']
+
 export default function StepperWithContent() {
   const [activeStep, setActiveStep] = useState(0);
   const [isLastStep, setIsLastStep] = useState(false);
   const [isFirstStep, setIsFirstStep] = useState(false);
-  const [step, setStep] = useState([-1, -1]);	// [스토리/장르, 성별]
+  const [step, setStep] = useState(new Array(db.count).fill(-1));	// [스토리/장르, 성별]
   const [webcamLoaded, setWebcamLoaded] = useState(false);
   const [imgSrc, setImgSrc] = useState(null);
   const [queuestataus, setQueueStatus] = useState(0);
@@ -23,19 +27,18 @@ export default function StepperWithContent() {
   const [index, setIndex] = useState(0); // 현재 이미지 인덱스 상태
   const [backgroundOpacity, setBackgroundOpacity] = useState(0);
   const webcamRef = useRef(null);
-
-  const capture = useCallback(() => setImgSrc(webcamRef.current.getScreenshot({ width: 640, height: 480 })), [webcamRef]);
   const router = useRouter();
+  const capture = useCallback(() => setImgSrc(webcamRef.current.getScreenshot({ width: 640, height: 480 })), [webcamRef]);
 
   const url = endpoint => `${process.env.NODE_ENV == 'development' ? 'http://216.153.62.1:8080' : ''}/api/${endpoint}`;
-  const backgroundImages = ['/transparent.png', '/autumn.jpg', '/dawn.jpg', '/snow.jpg', '/space.jpg']
   const handleNext = () => !isLastStep && setActiveStep(cur => cur + 1);
   const handlePrev = () => !isFirstStep && setActiveStep(cur => cur - 1);
   const generateImages = async () => {
     router.push('/loading')
     const formData = new FormData();
     formData.append('files', new File([await fetch(imgSrc).then(res => res.blob())], "image.jpg", { type: "image/jpeg" }));
-    ['story', 'gender'].forEach((key, i) => formData.append(key, step[i]));
+    formData.append('db',db)
+    Object.keys(db).forEach((key, i) => formData.append(key, step[i]));
     const taskId = +(await fetch(url('generateImages/1'), { method: 'POST', body: formData }).then(ret => ret.json())).taskId
     console.log(`taskId: ${taskId}`);
     const intervalId = setInterval(async () => {
@@ -66,7 +69,7 @@ export default function StepperWithContent() {
 
         <div className="w-full px-24 py-4">
           <Stepper activeStep={activeStep} isLastStep={setIsLastStep} isFirstStep={setIsFirstStep}>
-            {[[CameraIcon, "사진"], [PaintBrushIcon, "스토리"], [UserIcon, "성별"]].map(([Icon, str], i) =>
+            {steps.map(([Icon, str], i) =>
               <Step key={i} onClick={() => setActiveStep(i)}>
                 <Icon className="h-5 w-5" />
                 <div className="absolute -bottom-[4.5rem] w-max text-center">
@@ -96,36 +99,36 @@ export default function StepperWithContent() {
                 </Button>
               </div>
             }
-            {[["디즈니", "지브리", "해리포터"], ["남자", "여자"]].map((li, i1) =>
+            {Object.values(db).map((li, i1) =>
               // step
-              activeStep == i1 + 1 && 
-                <div className="flex justify-center p-4">
-                  {li.map((str, i2) =>
-                    <Card className="mx-auto mt-6 w-96 ">
-                      <CardHeader color="blue-gray" className="relative h-56">
-                        <img
-                          src={backgroundImages[i2+1]}
-                          alt="card-image"
-                        />
-                      </CardHeader>
-                      <CardBody>
-                        <Typography variant="h5" color="blue-gray" className="mb-2">
-                          {str}
-                        </Typography>
-                        <Typography>
-                          The place is close to Barceloneta Beach and bus stop just 2 min by
-                          walk and near to &quot;Naviglio&quot; where you can enjoy the main
-                          night life in Barcelona.
-                        </Typography>
-                      </CardBody>
-                      <CardFooter className="pt-0">
-                        <Button key={`${i1}${i2}`} onClick={() => setStep({ ...step, [i1]: i2 })} onMouseEnter={() => setIndex(i2 + 1)} color={step[i1] == i2 ? "blue" : "gray"}>
-                          {str}
-                        </Button>
-                      </CardFooter>
-                    </Card>
-                  )}
-                </div>
+              activeStep == i1 + 1 &&
+              <div className="flex justify-center p-4">
+                {li.map((str, i2) =>
+                  <Card className="mx-auto mt-6 w-96 ">
+                    <CardHeader color="blue-gray" className="relative h-56">
+                      <img
+                        src={backgroundImages[i2 + 1]}
+                        alt="card-image"
+                      />
+                    </CardHeader>
+                    <CardBody>
+                      <Typography variant="h5" color="blue-gray" className="mb-2">
+                        {str}
+                      </Typography>
+                      <Typography>
+                        The place is close to Barceloneta Beach and bus stop just 2 min by
+                        walk and near to &quot;Naviglio&quot; where you can enjoy the main
+                        night life in Barcelona.
+                      </Typography>
+                    </CardBody>
+                    <CardFooter className="pt-0">
+                      <Button key={`${i1}${i2}`} onClick={() => setStep({ ...step, [i1]: i2 })} onMouseEnter={() => setIndex(i2 + 1)} color={step[i1] == i2 ? "blue" : "gray"}>
+                        {str}
+                      </Button>
+                    </CardFooter>
+                  </Card>
+                )}
+              </div>
 
             )}
           </div>
