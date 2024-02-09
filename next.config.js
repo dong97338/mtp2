09418abec01712cb4bbdcc549ee04a558c787805
dong97338 <dong97338@gmail.com>
2024-02-09 14:07:07 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,22 @@
 /** @type {import('next').NextConfig} */
 const { API_HOST, API_PORT } = process.env;
-console.log(`API_HOST: ${API_HOST}, PORT: ${API_PORT}`);
 module.exports = {
-  async rewrites() {
-    return [
+  rewrites: async () =>
+    [
       {
         source: '/api/:path*',
         destination: `http://${API_HOST}:${API_PORT}/api/:path*`, //next.config.js에서는 :path*를 사용함에 주의
       },
-    ]
+    ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
   },
-	images: {
-		remotePatterns: [
-		  {
-			protocol: 'https',
-			hostname: 'images.unsplash.com',
-			port: '',
-			pathname: '**',
-		  },
-		],
-	  },
   reactStrictMode: true,
 }
